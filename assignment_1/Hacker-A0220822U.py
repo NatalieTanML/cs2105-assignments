8225a3a4e320bd02f286f25e50e2eb1a36c1a6ca
NatalieTanML <nat130613@gmail.com>
2022-02-12 21:59:53 +0800

    A1: done

--- assignment_1/Hacker-A0220822U.py
+++ assignment_1/Hacker-A0220822U.py
@@ -1,15 +1,72 @@
+import sys
+import hashlib
 from socket import *
 
-serverName = '137.132.92.111'
-serverPort = 4444
+# constants
+SERVER_NAME = '137.132.92.111'
+SERVER_PORT = 4444
 
-clientSocket = socket(AF_INET, SOCK_STREAM)
-handshake = "STID_427167"
-encodedHandshake = handshake.encode()
+METHOD_HANDSHAKE = "STID_" + sys.argv[1] # 427167
+METHOD_LOGIN = "LGIN_"
+METHOD_LOGOUT = "LOUT_"
+METHOD_GET = "GET__"
+METHOD_PUT = "PUT__"
+METHOD_BYE = "BYE__"
 
-clientSocket.connect((serverName, serverPort))
-clientSocket.send(encodedHandshake)
-response = clientSocket.recv(1024)
-print(response.decode())
+encodedHandshake = METHOD_HANDSHAKE.encode()
+encodedLogout = METHOD_LOGOUT.encode()
+encodedGet = METHOD_GET.encode()
+encodedBye = METHOD_BYE.encode()
 
-clientSocket.close()
+def hash(data):
+    request = METHOD_PUT + str(hashlib.md5(data).hexdigest())
+    return request.encode()
+
+def guess(password):
+    request = METHOD_LOGIN + str(password).zfill(4)
+    return request.encode()
+
+def main():
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((SERVER_NAME, SERVER_PORT))
+
+    # handshake
+    clientSocket.send(encodedHandshake)
+    handshake = clientSocket.recv(4)
+
+    if handshake != b'200_':
+        clientSocket.close()
+        return
+
+    # connected
+    for i in range(10000):
+        encodedLogin = guess(i)
+        clientSocket.send(encodedLogin)
+        response = clientSocket.recv(4)
+        if response == b'201_':
+            # successful login
+            clientSocket.send(encodedGet)
+            response = clientSocket.recv(4)
+            if response == b'100_':
+                # get the size
+                size_b = b''
+                while True:
+                    byte = clientSocket.recv(1)
+                    if byte == b'_':
+                        break
+                    size_b += byte
+
+                # write hash
+                size = int(size_b)
+                data = clientSocket.recv(size)
+                encodedHash = hash(data)
+                clientSocket.send(encodedHash)
+
+                clientSocket.recv(4)
+                clientSocket.send(encodedLogout)
+                clientSocket.recv(4)
+
+    clientSocket.send(encodedBye)
+
+if __name__ == "__main__":
+    main()
